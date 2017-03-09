2fc9a691a40e4c677eafa1f8924a4352b93e93fe
조영욱 <Mac@YoungWook-MacBook-Pro.local>
2017-03-09 14:10:00 +0900

    s1

--- app/src/main/java/examples/com/myapplication1/MainActivity.java
+++ app/src/main/java/examples/com/myapplication1/MainActivity.java
@@ -1,13 +1,27 @@
 package examples.com.myapplication1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
